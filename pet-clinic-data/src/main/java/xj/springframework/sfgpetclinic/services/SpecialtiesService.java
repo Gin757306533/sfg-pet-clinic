da411313961d8b29658508a6c1e741f2e12efdfc
xujie <jie.xu@thoughtworks.com>
2021-09-11 15:08:18 +0800

    added specialties map impl , close #31, spring framework guru , section 7, 125

---
+++ pet-clinic-data/src/main/java/xj/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package xj.springframework.sfgpetclinic.services;
+
+import xj.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
